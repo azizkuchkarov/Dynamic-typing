68c4cbcf900d905be000b9dff52865ea9e9a8315
azizkuchkarov <aziz_kuchkarov91@mail.ru>
2022-12-27 08:54:06 +0500

    Working with Dynamic typing

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log(typeof String(4));
+console.log(typeof ("aziz" + 21));
+console.log(typeof Number("5"));
+console.log(typeof +"5");
+console.log(typeof parseInt("15"));
+
+const aziz = 0;
+if (aziz == false) {
+  console.log("Kuchkarov Aziz");
+}
+
+console.log(typeof Boolean("4"));
